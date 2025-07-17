a63f0b20dffc5c0b0a5c25e221e3d788f4975609
yizhou <yizzhao@nvidia.com>
2025-07-16 17:19:01 -0700

    zero output

--- 7-depth/src/main.py
+++ 7-depth/src/main.py
@@ -9,14 +9,16 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 TRT_LOGGER = trt.Logger()
+INPUT_WIDTH = 480
+INPUT_HEIGHT = 288
 
 # Filenames of TensorRT plan file and input/output images.
-engine_file = "/home/yizhou/workspaces/isaac_ros-dev/isaac_ros_assets/models/dnn_stereo_disparity/dnn_stereo_disparity_v4.1.0_onnx/ess.engine"
+engine_file = "/home/yizhou/workspaces/isaac_ros-dev/isaac_ros_assets/models/dnn_stereo_disparity/dnn_stereo_disparity_v4.1.0_onnx/light_ess.engine"
 plugin_path = "/home/yizhou/workspaces/isaac_ros-dev/isaac_ros_assets/models/dnn_stereo_disparity/dnn_stereo_disparity_v4.1.0_onnx/plugins/x86_64/ess_plugins.so"
 
 output_file = "./image/disparity.png"
 
-def load_and_resize_image(image_path, target_size=(960, 576)):
+def load_and_resize_image(image_path, target_size=(INPUT_WIDTH, INPUT_HEIGHT)):
     """
     Load an image from path and resize it to the target size.
     
@@ -34,6 +36,7 @@ def load_and_resize_image(image_path, target_size=(960, 576)):
     resized_image = image.resize(target_size, Image.Resampling.LANCZOS)
     print(f"Resized image size: {resized_image.size}")
     
+    # return np.expand_dims(np.moveaxis(np.array(resized_image), 2, 0), 0)
     return np.moveaxis(np.array(resized_image), 2, 0)
 
 
@@ -51,22 +54,20 @@ def load_engine(engine_file_path, plugin_path=None):
         return runtime.deserialize_cuda_engine(f.read())
 
 def main():
-    print("Running TensorRT inference for FCN-ResNet101")
+    print("Running TensorRT inference for ESS Stereo Disparity")
     
     # Example: Load and resize images
     print("\n--- Image Loading and Resizing Example ---")
     
     # Load and resize the left image
-    left_image = load_and_resize_image("./image/left.png", target_size=(960, 576))
+    left_image = load_and_resize_image("./image/left.png", target_size=(INPUT_WIDTH, INPUT_HEIGHT))
     print(f"Successfully loaded and resized left image to {left_image.size}")
-    # You can save the resized image if needed
-    # left_image.save("./image/left_resized.png")
     
     # Load and resize the right image
-    right_image = load_and_resize_image("./image/right.png", target_size=(960, 576))
+    right_image = load_and_resize_image("./image/right.png", target_size=(INPUT_WIDTH, INPUT_HEIGHT))
     print(f"Successfully loaded and resized right image to {right_image.size}")
-    # You can save the resized image if needed
-    # right_image.save("./image/right_resized.png")
+
+    # import ipdb; ipdb.set_trace()
     
     print("\n--- TensorRT Inference ---")
     with load_engine(engine_file, plugin_path) as engine:
@@ -82,13 +83,13 @@ def main():
             for tensor in tensor_names:
                 size = trt.volume(context.get_tensor_shape(tensor))
                 dtype = trt.nptype(engine.get_tensor_dtype(tensor))
-
+                print(f"Tensor: {tensor}, Size: {size}, Dtype: {dtype}, Mode: {engine.get_tensor_mode(tensor)}")
                 
                 if engine.get_tensor_mode(tensor) == trt.TensorIOMode.INPUT:
-                    context.set_input_shape(tensor, (1, 3, 576, 960))
+                    context.set_input_shape(tensor, (1, 3, INPUT_HEIGHT, INPUT_WIDTH))
                     input_image = left_image if tensor == "input_left" else right_image
-                    input_buffer[tensor] = np.ascontiguousarray(input_image)
-                    input_memory[tensor] = cuda.mem_alloc(input_image.nbytes)
+                    input_buffer[tensor] = np.ascontiguousarray(input_image.astype(np.float32))
+                    input_memory[tensor] = cuda.mem_alloc(input_buffer[tensor].nbytes)
                     context.set_tensor_address(tensor, int(input_memory[tensor]))
                 else:
                     output_buffer[tensor] = cuda.pagelocked_empty(size, dtype)
@@ -97,32 +98,27 @@ def main():
 
             stream = cuda.Stream()
 
-            
-
             # Transfer input data to the GPU.
-            cuda.memcpy_htod_async(input_memory["input_left"], input_buffer["input_left"], stream)
-            cuda.memcpy_htod_async(input_memory["input_right"], input_buffer["input_right"], stream)
+            for tensor_name in input_buffer.keys():
+                cuda.memcpy_htod_async(input_memory[tensor_name], input_buffer[tensor_name], stream)
             
             # Run inference
             context.execute_async_v3(stream_handle=stream.handle)
 
-            import ipdb; ipdb.set_trace()
-
             # Transfer prediction output from the GPU.
-            cuda.memcpy_dtoh_async(output_buffer["output_left"], output_memory["output_left"], stream)
-            cuda.memcpy_dtoh_async(output_buffer["output_conf"], output_memory["output_conf"], stream)
-            
-            import ipdb; ipdb.set_trace()
-            
+            for tensor_name in output_buffer.keys():
+                cuda.memcpy_dtoh_async(output_buffer[tensor_name], output_memory[tensor_name], stream)
 
             stream.synchronize()
             output_d64 = np.array(output_buffer["output_left"], dtype=np.int64)
             # np.savetxt('test.out', output_d64.astype(int), fmt='%i', delimiter=' ', newline=' ')
-            img = np.reshape(output_d64, (576, 960))
+            img = np.reshape(output_d64, (INPUT_HEIGHT, INPUT_WIDTH))
             print("Writing output image to file {}".format(output_file))
             # transfer the numpy array to uint8 and save it as grayscale png
             Image.fromarray(img.astype(np.uint8)).save(output_file)
-            
+
+            import ipdb; ipdb.set_trace()
+
         # print("Execution context created successfully")
         # input_data = np.random.randn(1, 3, 224, 224).astype(np.float32)
         # output_data = np.empty((1, 1000), dtype=np.float32)
